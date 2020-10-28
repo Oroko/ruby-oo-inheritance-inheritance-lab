f5654cd107aced5176e2330fd34c2f51527085c4
Oroko <aoroko09@gmail.com>
2020-10-28 18:45:08 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+# frozen_string_literal: true
 
+class Student < User
+  def initialize
+    super
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+  def knowledge
+    @knowledge
+  end
 end
